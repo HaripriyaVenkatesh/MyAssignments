4689e0e655bcfae23aa34aef0890e064fd322f46
HaripriyaVenkatesh <haripriyavenkatesh25@gmail.com>
2026-09-08 18:10:10 +0530

    Scope

--- scope.js
+++ scope.js
@@ -4,8 +4,8 @@ function getBrowserVersion()
     console.log("Global value:", browserVersion )
     if(browserVersion==="Chrome")
     {
-        var localbrowserVersion="Chrome 120"//var browserVersion="Chrome 120" var localbrowserVersion="Chrome 120"
-        console.log("Inside if block:" , localbrowserVersion); //localbrowserVersion
+        var localbrowserVersion="Chrome 120"
+        console.log("Inside if block:" , localbrowserVersion); 
         
     }
 
