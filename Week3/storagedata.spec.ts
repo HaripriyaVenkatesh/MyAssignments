bd7c91aa58638842c5320f44e11c087acd363aa0
HaripriyaVenkatesh <haripriyavenkatesh25@gmail.com>
2026-09-21 14:09:10 +0530

    Add files via upload

---
+++ Week3/storagedata.spec.ts
@@ -0,0 +1,19 @@
+import {test} from "@playwright/test"
+
+
+test.use(
+    {
+       storageState:'Data/sflogin.json' 
+    }
+)
+
+test('auth file to skip the login', async ({page}) => {
+
+await page.goto("https://leaftaps.com/opentaps/control/login")
+
+await page.waitForLoadState('domcontentloaded')
+
+console.log(await page.title());
+
+
+})
